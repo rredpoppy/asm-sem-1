0128840454f41d5c522da4f436e2e136efd6fde4
Adrian Rosian <adrian.rosian@gmail.com>
2023-10-20 04:42:21 +0000

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,55 +1,50 @@
-; a - byte, b - word, c - double word, d - qword - Signed representation
-; (b+c+d)-(d+a)
-section .text
-    extern printf
-    extern exit
-    global _start
+bits 32 ;asamblare si compilare pentru arhitectura de 32 biti
+; definim punctul de intrare in programul principal
+global  start 
 
-_start:
-    push    ebp           ; Save the base pointer
+extern  exit ; indicam asamblorului ca exit exista, chiar daca noi nu o vom defini
+import  exit msvcrt.dll; exit este o functie care incheie procesul, este definita in msvcrt.dll
+        ; msvcrt.dll contine exit, printf si toate celelalte functii C-runtime importante
+segment  data use32 class=data ; segmentul de date in care se vor defini variabilele 
+	a db 0x3
+    b dw 0xfded ; -0x213 base 10
+    c dd 0x23e76
+    d dq 0x11764f
+    ; (b + c + d) - (a + d) = 0x23c60
+segment  code use32 class=code ; segmentul de cod
+start: 
+
+	push    ebp           ; Save the base pointer
     mov     ebp, esp      ; Set the base pointer
 
     ; ( b + c + d )
     xor eax, eax ; clears eax
-    mov [b], ax
+    mov ax, [b]
     cwde ; extend b to double while keeping sign
     add eax, [c] ; b + c
+    cdq ; edx now holds the sign of b + c upper dword for use later
     add eax, [d] ; add the least significant dword of d
     push eax ; save least significant dword of result to stack
-    mov [d+32], eax ; move the higher dword of d into eax
-    adc eax, 0 ; add the carry to eax; eax:ebx now holds the higer dword of the result
+    mov eax, [d+32] ; move the higher dword of d into eax
+    adc eax, edx ; add the carry to eax + edx (signed b + c upper dword); eax:ebx now holds the higer dword of the result; the edx part is necessary to maintain the sign of b + c
     push eax ; save higher dword to stack
 
     ; (d + a)
     xor eax, eax ; reset eax
-    mov [a], al ; sign extend eax
+    mov al, [a] ; sign extend eax
     cbw
     cwde
     add eax, [d] ; add lower dword of d to eax
-    pop ebx ; retrieve higher dword of previous result
     push eax ; push lower dword of current result
-    mov [d+32], eax ; load higher dword of d
-    adc eax, ebx ; add the higher dwords of the result
-    pop ebx ; retrieve lover dword of current result
-    pop ecx ; retrieve lower dword of previous result
-    sbb ebx, ecx ; add them together
-    adc eax, 0 ; add carry
+    mov eax, [d+32] ; load higher dword of d
+    adc eax, 0 ; add the carry to higher dword
+    pop ecx ; retrieve lower dword of current result
+    pop ebx ; retrieve higher dword of previous result
+    pop edx ; retrieve the lower dword of previous result
+    sub ecx, edx ; substract with possible carry the lower dwords
+    sbb eax, ebx ; substract higher dwords with carry; result is now eax:ecx
 
-
-
-    push    dword [a]     ; Push the address of the integer onto the stack
-    push    dword fmt     ; Push the address of the format string onto the stack
-    call    printf        ; Call the printf function
-    add     esp, 8        ; Clean up the stack
-
-    mov eax, 0xa
-    call exit
-
-    pop ebp               ; Restore stack
-
-section .data
-    a db 0x3
-    b dw -0x213
-    c dd 0x23e76
-    d dq 0x11764f
-    fmt db "EAX: #%08X", 10, 0
+	push   dword 0 ;exit code fn
+	call   [exit]
+    
+    pop ebp ; restoring base pointer
